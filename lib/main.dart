c9b6c6523f020baaab5ac4f54092931455444ad9
jules <jules.hermans@student.kuleuven.be>
2020-11-21 11:36:45 +0100

    sprint 1

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                flex: 5,
+                child: Center(
+                  child: Text(
+                    'the Questions of the quiz should display here later!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                )),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: FlatButton(
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    child: Text(
+                      'TRUE',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      print('True clicked! ');
+                    }),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: FlatButton(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child: Text(
+                      'FALSE',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      print('False clicked! ');
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
